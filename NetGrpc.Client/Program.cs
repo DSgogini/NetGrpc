e767a759e5ac81f29403c57c9c3217d1cafe869c
Dinesh Singh Gogini <goginidineshsingh0112@hotmail.com>
2021-06-12 19:02:28 +0530

    Initial Commit.

---
+++ NetGrpc.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetGrpc.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
